d2ce0e85af1ed8e5128654d437cfa3e25d381f6e
Rihards <rg2055@github>
2026-03-31 13:14:11 +0300

    Add mobile PWA install CTA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.42';
+const CACHE = 'minka-4.1.43';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
