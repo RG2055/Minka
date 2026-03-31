11df8662f17489ab7e12fe29bfc040de16fdb970
Rihards <rg2055@github>
2026-03-31 12:05:14 +0300

    Make mobile calendar full-bleed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.25';
+const CACHE = 'minka-4.1.26';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
