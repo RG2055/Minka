7166d760f6c1fdd354d86223653216184f0bd953
Rihards <rg2055@github>
2026-03-31 19:57:30 +0300

    Localize mobile dock labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.50';
+const CACHE = 'minka-4.1.51';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
