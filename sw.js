bb7b7d74ac4b8bbd1971797470459395a9136a64
Rihards <rg2055@github>
2026-03-31 12:46:44 +0300

    Restore mobile nameday chip and search input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.38';
+const CACHE = 'minka-4.1.39';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
