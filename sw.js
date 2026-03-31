c101b111f86f26070ea1d393ecb77eb16c5b9976
Rihards <rg2055@github>
2026-03-31 12:40:36 +0300

    Fit mobile night split cards to screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.36';
+const CACHE = 'minka-4.1.37';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
