7917b07d7b63a59661c900e2833102f8f816b872
Rihards <rg2055@github>
2026-03-31 11:51:20 +0300

    Move mobile month selector to right side

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.20';
+const CACHE = 'minka-4.1.21';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
