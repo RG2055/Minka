3a04338cc1a2070bdb4ce9540f7ad79ba82fbc80
Rihards <rg2055@github>
2026-03-31 12:33:09 +0300

    Show bolus time and history on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.32';
+const CACHE = 'minka-4.1.33';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
