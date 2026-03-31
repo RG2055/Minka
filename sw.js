6f6faaa7e6ac33975600fbac0ec1556877c05f4c
Rihards <rg2055@github>
2026-03-31 12:34:39 +0300

    Fix mobile night split card overlap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.33';
+const CACHE = 'minka-4.1.34';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
