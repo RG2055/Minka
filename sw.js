5ed06b9ad6de87298cb20f3010d83929c9d12a8b
Rihards <rg2055@github>
2026-03-31 20:05:55 +0300

    Hide inner mobile calendar loader

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.53';
+const CACHE = 'minka-4.1.54';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
