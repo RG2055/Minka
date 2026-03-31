40f18f999c30ddfc63db333328e1d37ee19f02e0
Rihards <rg2055@github>
2026-03-31 12:50:06 +0300

    Restore bolus workers and mobile actions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.39';
+const CACHE = 'minka-4.1.40';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
