7a9ec962cc2fccc5d1e82b8d01a17ee1094db66c
Rihards <rg2055@github>
2026-03-31 12:37:42 +0300

    Shrink mobile night split cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.34';
+const CACHE = 'minka-4.1.35';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
