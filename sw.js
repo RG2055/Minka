9925c4f3848618e48938d093a711d3749038d6dd
Rihards <rg2055@github>
2026-03-31 19:53:42 +0300

    Fix mobile search bar overlap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.48';
+const CACHE = 'minka-4.1.49';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
