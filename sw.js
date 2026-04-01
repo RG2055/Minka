b3f266fe0a3ebaf1114f6bd90b7956a720706a56
Rihards <rg2055@github>
2026-04-01 08:46:32 +0300

    Hide finished shift cards for today

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.66';
+const CACHE = 'minka-4.1.67';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
