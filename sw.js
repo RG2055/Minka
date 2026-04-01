265b1a4cac3ddcef8de485bdb47fbdec98d1ea8e
Rihards <rg2055@github>
2026-04-02 00:48:57 +0300

    Share mobile login with iPhone web app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.73';
+const CACHE = 'minka-4.1.74';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
