b4c4945ac9f8c2e39bb86bd0967b9fe7c3fa491f
Rihards <rg2055@github>
2026-04-01 20:04:35 +0300

    Bump cache for stars sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.68';
+const CACHE = 'minka-4.1.69';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
