0e82bd0383022f50cf31898a25466afc52d567aa
Rihards <rg2055@github>
2026-04-01 19:44:55 +0300

    Bump cache for night sky overlay

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.67';
+const CACHE = 'minka-4.1.68';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
