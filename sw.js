2f13651c334c4b9cb2f9f12a77741b88b8613ad9
Rihards <gamernr1elite@gmail.com>
2026-04-08 15:57:52 +0300

    Bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.78';
+const CACHE = 'minka-4.1.79';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
