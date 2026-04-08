aa5c69bbb494fc6e44907f33ea4fc4a69d2f9cd8
Rihards <gamernr1elite@gmail.com>
2026-04-08 03:20:48 +0300

    Bump app cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.1.76';
+const CACHE = 'minka-4.1.77';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
