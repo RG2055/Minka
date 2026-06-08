279bf5a03e7084d8c0236e9605d037156fb71f04
Rihards <rihards@example.local>
2026-06-08 22:35:38 +0300

    Add Lacitis launcher

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.3.92';
+const CACHE = 'minka-4.3.93';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
