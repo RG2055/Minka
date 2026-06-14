d36d2f677f3661171e821c458fec3449420f893e
Rihards <rihards@example.local>
2026-06-14 19:33:36 +0300

    Add pusdienas launcher

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.4.07';
+const CACHE = 'minka-4.4.08';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
