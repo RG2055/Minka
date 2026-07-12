7fa61a6a9c9c1f2c968d973d950845db6bff65ea
Rihards <rihards@example.local>
2026-07-13 00:05:54 +0300

    Polish mobile controls and card loading state

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.0';
+const CACHE = 'minka-4.6.1';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
