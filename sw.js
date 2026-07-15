06fc48296f3fe89be374e771a3d10a3268fc2d49
Rihards <rihards@example.local>
2026-07-15 21:36:19 +0300

    Align next shift panels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.20';
+const CACHE = 'minka-4.6.21';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
