d17f54a20e10e44560a20f192b2bcbfc8e1dcc26
Rihards <rihards@example.local>
2026-07-22 13:25:38 +0300

    Reduce Lacitis resource usage on low-end PCs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.32';
+const CACHE = 'minka-4.6.33';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
