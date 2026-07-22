e31f6a389d17787dfd8d3736bbe9003e3f0f0c9b
Rihards <rihards@example.local>
2026-07-22 15:12:01 +0300

    Optimize shell performance and stop radio for Lacitis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.34';
+const CACHE = 'minka-4.6.35';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
