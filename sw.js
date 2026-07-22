01269138b68820d8c507f08aad44e0886e275380
Rihards <rihards@example.local>
2026-07-22 21:56:04 +0300

    Add favorites and custom playlists to Lacitis mini

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.42';
+const CACHE = 'minka-4.6.43';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
