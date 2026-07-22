fe381de8521c805f535e48fdbf4ed459e3ca2187
Rihards <rihards@example.local>
2026-07-22 14:48:28 +0300

    Improve Lacitis mini player navigation and fullscreen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.33';
+const CACHE = 'minka-4.6.34';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
