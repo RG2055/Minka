2b208ac271f6510c26e184abe2ec391dd24361f3
Rihards <rihards@example.local>
2026-07-22 21:59:48 +0300

    Refresh mini profile workers after calendar load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.43';
+const CACHE = 'minka-4.6.44';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
