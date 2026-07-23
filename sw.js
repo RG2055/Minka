07e26a4ca9c274de420b33587937557bac647039
Rihards <rihards@example.local>
2026-07-23 10:51:59 +0300

    Trigger Pages deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.46';
+const CACHE = 'minka-4.6.47';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
