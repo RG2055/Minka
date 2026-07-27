50200139be4c8df6dca2ca43b7d48a7aca501e1b
Rihards <rihards@example.local>
2026-07-27 22:42:37 +0300

    Redesign radio skin and color picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.73';
+const CACHE = 'minka-4.6.81';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
