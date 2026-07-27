eccf473e8ad4a5a41ddffd8eed00bb7cf4cc5232
Rihards <rihards@example.local>
2026-07-27 12:05:59 +0300

    Bump service worker cache for month selector fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.61';
+const CACHE = 'minka-4.6.62';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
