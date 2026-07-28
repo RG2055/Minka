79753cc49dccb4a3fc016f888fa54420d1137194
Rihards <rihards@example.local>
2026-07-28 19:34:17 +0300

    Keep next-day side cards compact

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.99';
+const CACHE = 'minka-4.6.100';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
