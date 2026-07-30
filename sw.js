e066df63147bd03376e674efbdb128c689db23a8
Rihards <rihards@example.local>
2026-07-30 22:00:57 +0300

    Fix month rollover and refine skin picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.105';
+const CACHE = 'minka-4.6.109-month-calendar-year';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
