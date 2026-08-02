4502938bed1eb42336145e1e839e539a151c4615
Rihards <rihards@example.local>
2026-08-02 19:01:43 +0300

    Polish calendar header and buddy layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.115-coffee-sync-restore';
+const CACHE = 'minka-4.6.116-header-buddy-polish';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
