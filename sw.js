19c5b551028d7d61b7f3e9503eca863c3a1d8523
Rihards <rihards@example.local>
2026-08-08 17:32:51 +0300

    Revert "Polish calendar header and refresh PWA cache"
    This reverts commit 54c1a3b40c8e4a3cff3629017c5824bbe906a089, reversing
    changes made to 6ba2f555bef140a0ed653700b381e1c7486efe30.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.143-glass5';
+const CACHE = 'minka-4.6.133-win-dpi-icons';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
