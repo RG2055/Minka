d7364eb453f6085f5858a16a7f23981af5843d02
Rihards <rihards@example.local>
2026-08-09 00:17:21 +0300

    Polish feedback selector

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.169-feedback39';
+const CACHE = 'minka-4.6.173-feedback43';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
