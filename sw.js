71360c5f34354e5f47fd216f68dffbccd0c58372
Rihards <rihards@example.local>
2026-08-10 15:52:12 +0300

    Redesign community chat and comments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.195-cardframe2';
+const CACHE = 'minka-4.6.203-community8';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
