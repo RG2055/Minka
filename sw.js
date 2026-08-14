fe6c4d9295610be94e9a492e8d8c377e5ed7da40
Rihards <rihards@example.local>
2026-08-14 17:39:05 +0300

    Remove night split room panel shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.294-nightsplit-cat-layout';
+const CACHE = 'minka-4.6.295-nightsplit-flat-rooms';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
