9cda2fac40a6120dc6f5b8dd247495fd5921a1f7
Rihards <rihards@example.local>
2026-08-14 17:06:10 +0300

    Fix night split rhythm labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.291-nightsplit';
+const CACHE = 'minka-4.6.292-nightsplit-labels';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
