43013ea9c117e40ed080813bc95fc268865c728f
Rihards <rihards@example.local>
2026-08-14 16:57:17 +0300

    Redesign night split and bump PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.290-brand1';
+const CACHE = 'minka-4.6.291-nightsplit';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
