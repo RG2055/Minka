618fe60c93ca86aa6a742543d61cf8b9a8aeae95
Rihards <rihards@example.local>
2026-08-14 17:19:45 +0300

    Keep night split labels inside PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.292-nightsplit-labels';
+const CACHE = 'minka-4.6.293-nightsplit-gutter';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
