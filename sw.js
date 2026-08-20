e052ca80a1a945f1e8dcb24b6fa519742c29824f
Rihards <rihards@example.local>
2026-08-20 19:19:10 +0300

    Atjauno Fluent mood stikla skatu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.426-noto13';
+const CACHE = 'minka-4.6.428-fluentmood16';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
