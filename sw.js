76598b7ec5f4d20ee57050f3f8c4b70e37d37080
Rihards <rihards@example.local>
2026-08-22 18:40:41 +0300

    feat: improve mood bubble depth and fatigue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.439-emoji-particles20';
+const CACHE = 'minka-4.6.440-mood-bubbles31';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
