fc217382377ad231b99b99af6ba729349a235b6b
Rihards <rihards@example.local>
2026-08-22 14:41:01 +0300

    Pievieno 08:00 maiņas emoji efektus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.428-fluentmood16';
+const CACHE = 'minka-4.6.439-emoji-particles20';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
