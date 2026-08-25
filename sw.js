39273c36ed200dcf0218187725eb52ddaf2c3024
Rihards <rihards@example.local>
2026-08-25 14:17:10 +0300

    feat: compact header night split

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.466-remove-window-controls-overlay';
+const CACHE = 'minka-4.6.467-compact-fluent-night-split';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
