7c3c7781f27ae4366fb66af01f5e3daba2e6d77e
Rihards <rihards@example.local>
2026-08-28 22:37:41 +0300

    Fix roster section headings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.502';
+const CACHE = 'minka-4.6.503';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
