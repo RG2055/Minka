41dfa248ec83afc3f7a318a9fe2deffc19544129
Rihards <rihards@example.local>
2026-08-30 20:51:10 +0300

    Revamp card appearance picker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.523';
+const CACHE = 'minka-4.6.524';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
