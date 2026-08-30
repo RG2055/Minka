5bdc2a54dbae497b7761b037f96909df253e5821
Rihards <rihards@example.local>
2026-08-30 21:08:03 +0300

    Store scenic card backgrounds as images

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.524';
+const CACHE = 'minka-4.6.525';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
