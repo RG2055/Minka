d59b7349dc7a3cfc154e65a0b972bfa897d729be
Rihards <rihards@example.local>
2026-09-03 21:00:30 +0300

    Add optimized card skins and decorations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.554';
+const CACHE = 'minka-4.6.555';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
