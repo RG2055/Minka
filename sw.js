497579612165bf22fcc53f1cd27d4789b05bc8a1
Rihards <rihards@example.local>
2026-09-08 23:36:47 +0300

    Hide dashboard scrollbars and match fatigue glass to mood lemon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minka-4.6.589';
+const CACHE = 'minka-4.6.590';
 const APP_ROOT = new URL('./', self.registration.scope);
 const appUrl = relativePath => new URL(relativePath, APP_ROOT).href;
 
